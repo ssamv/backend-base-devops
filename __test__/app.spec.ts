945940032356144737b304cd4adb2245d8e48654
Sebastian MV <ssamv.1998@gmail.com>
2024-09-11 23:09:22 -0300

    test

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -1,9 +1,34 @@
 import { describe, test, expect } from "@jest/globals";
 import app from "../src/server.js";
+import { esPalindromo } from "../src/palindromo.js";
 import request from "supertest";
 import { configuration } from "../src/config.js";
 
 describe("Test Suite App", () => {
+
+    test("palindromo",()=>{
+        let frase: any = "ala";
+        expect(esPalindromo(frase)).toBe(true);
+
+        frase = "cama";
+        expect(esPalindromo(frase)).toBe(false);
+    });
+
+    /*test("sumar dos numeros", () => {
+
+        let a: any = 100;
+        let b: any = 200;
+        expect(suma(a, b)).toBe(300);
+
+        a = 10;
+        b = "a";
+        expect(suma(a, b)).toBeNaN();
+
+        a = undefined;
+        b = 1;
+        expect(() => { suma(a, b) }).toThrow("No se puede sumar indefinidos");
+
+    });*/
 
     test("endpoint /", () => {
         expect(1 + 1).toBe(2);
