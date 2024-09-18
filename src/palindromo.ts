444479c2a90818d2cf0aa2f716588cf53cbd7000
Sebastian MV <ssamv.1998@gmail.com>
2024-09-18 18:27:28 -0300

    fix issue "if cant be the only statement into else"

--- src/palindromo.ts
+++ src/palindromo.ts
@@ -1,13 +1,11 @@
 export const esPalindromo = (frase: string) => {
     if(typeof frase != "string"){
         throw new Error("El valor ingresado no corresponde a una cadena de texto");
+    }else if(frase == ""){
+        throw new Error("El texto ingresado esta vacio");
     }else{
-        if(frase == ""){
-            throw new Error("El texto ingresado esta vacio");
-        }else{
-            const fraseSinEspacios = frase.replace(/\s/g, "").toLowerCase();
-            const fraseInvertida = fraseSinEspacios.split("").reverse().join("");
-            return fraseSinEspacios === fraseInvertida;
-        }
+        const fraseSinEspacios = frase.replace(/\s/g, "").toLowerCase();
+        const fraseInvertida = fraseSinEspacios.split("").reverse().join("");
+        return fraseSinEspacios === fraseInvertida;
     }
 }
